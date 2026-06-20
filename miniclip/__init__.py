e78bb36f7592bd28c5b55d53843af2ab4b2f25cd
Prabal Bijoy Dutta <prabaldutta.iitr@gmail.com>
2026-06-20 13:19:59 -0700

    added imports in init

--- miniclip/__init__.py
+++ miniclip/__init__.py
@@ -3,8 +3,10 @@ from .temperature import LearnedTemperature
 from .loss import symmetric_infonce_loss
 from .synthetic import SyntheticPairs
 from .synthetic_text import SyntheticTextPairs
+from .synthetic_full import SyntheticImageTokens
 from .transformer import MultiHeadSelfAttention, TransformerBlock
 from .text_encoder import TextTransformer
+from .vit import VisionTransformer, PatchEmbed
 
 __all__ = [
     "ProjectionHead",
@@ -12,7 +14,10 @@ __all__ = [
     "symmetric_infonce_loss",
     "SyntheticPairs",
     "SyntheticTextPairs",
+    "SyntheticImageTokens",
     "MultiHeadSelfAttention",
     "TransformerBlock",
     "TextTransformer",
+    "VisionTransformer",
+    "PatchEmbed",
 ]
